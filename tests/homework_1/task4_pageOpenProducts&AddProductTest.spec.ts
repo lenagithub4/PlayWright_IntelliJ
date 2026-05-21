50ad1eca5fc35c92eaba8ca6372b04ecf6cb90fe
Jelena.Uscelemova <jelena.uscelemova@aaro.se>
2026-05-21 18:05:46 +0300

    Test N4: click menu Products & add new product

---
+++ tests/homework_1/task4_pageOpenProducts&AddProductTest.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../pages/LoginPage';
+
+test.beforeEach(async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.goto();
+    await loginPage.login('lenatest@aaro.com', 'LenaTest');
+
+    await expect(page.locator('h4')).toHaveText('Dashboard');
+});
+
+
+
+
+
+test('click menu Products & add new product', async ({ page }) => {
+    await page.getByTestId('nav-products').click();
+    await expect(page.locator('h4')).toHaveText('Products');
+
+    const productName = 'Apple';
+
+    // Fill product name
+    await page.getByLabel('Product Name').fill(productName);
+
+    // Click ADD button
+    await page.getByRole('button', { name: 'ADD' }).click();
+
+    // Verify product appears in the list
+    await expect(page.getByText(productName)).toBeVisible();
+    await expect(page.locator('li')).toContainText([productName]);
+
+});
+
